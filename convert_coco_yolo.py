e7906393be4e19a511e40fed1401a089f861c765
enes <enesozipek05@gmail.com>
2019-04-21 22:27:10 +0300

    custom training files init

---
+++ convert_coco_yolo.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+# best model until now
+import argparse
+import glob
+import os
+import sys
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path", help='Directory of json files containing annotations')
+    parser.add_argument(
+        "output_path", help='Output directory for image.txt files')
+    args = parser.parse_args()
+    json_files = sorted(glob.glob(os.path.join(args.path, '*.json')))
+    for json_file in json_files:
+        with open(json_file) as f:
+            data = json.load(f)
+            image = data['image']
+            annotations = data['annotation']
+            file_name = image['file_name']
+            width = float(image['width'])
+            height = float(image['height'])
+            converted_results = []
+            for ann in annotations:
+                cat_id = int(ann['category_id'])
+                left, top, bbox_width, bbox_height = map(float, ann['bbox'])
+                if cat_id == 18:
+                    cat_id = 4
+                elif cat_id == 91:
+                    cat_id = 5
+
+                # Yolo classes are starting from zero index
+                cat_id -= 1
+                x_center, y_center = (left + bbox_width / 2, top + bbox_height / 2)
+                # darknet expects relative values wrt image width&height
+                x_rel, y_rel = (x_center / width, y_center / height)
+                w_rel, h_rel = (bbox_width / width, bbox_height / height)
+                converted_results.append((cat_id,x_rel,y_rel,w_rel,h_rel))
+            with open(os.path.join(args.output_path,file_name+'.txt'), 'w+') as fp:
+                fp.write('\n'.join('%d %.6f %.6f %.6f %.6f' % res for res in converted_results))
---
+++ preprocess_flir_dataset.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+train_images="$HOME/Downloads/FLIR_ADAS_12_11_18/FLIR_ADAS/training/PreviewData"
+validation_images="$HOME/Downloads/FLIR_ADAS_12_11_18/FLIR_ADAS/validation/PreviewData"
+train_anns="$HOME/Downloads/FLIR_ADAS_12_11_18/FLIR_ADAS/training/Annotations"
+validation_anns="$HOME/Downloads/FLIR_ADAS_12_11_18/FLIR_ADAS/validation/Annotations"
+train_file="thermal_train.txt"
+valid_file="thermal_validation.txt"
+cfg_file="yolov3-thermal.cfg"
+data_file="thermal.data"
+name_file="thermal.names"
+image_dir="$PWD/darknet/build/darknet/x64/data/thermal"
+
+[ -f "$train_file" ] && rm "$train_file"
+[ -f "$valid_file" ] && rm "$valid_file"
+
+
+for value in {1..8862}
+do
+printf "data/thermal/FLIR_%05d.jpeg\n" $value >> "$train_file"
+done
+for value in {8863..10228}
+do
+printf "data/thermal/FLIR_%05d.jpeg\n" $value >> "$valid_file"
+done
+
+# Copy necessary files to correct directories
+cp "$cfg_file"   "$PWD/darknet/build/darknet/x64/"
+cp "$train_file" "$PWD/darknet/build/darknet/x64/data/"
+cp "$valid_file" "$PWD/darknet/build/darknet/x64/data/"
+cp "$data_file"  "$PWD/darknet/build/darknet/x64/data/"
+cp "$name_file"  "$PWD/darknet/build/darknet/x64/data/"
+
+# Copy images to correct directory
+rm -rf "$image_dir"
+mkdir "$image_dir"
+cp "$train_images/"* "$image_dir" 2>/dev/null
+cp "$validation_images/"* "$image_dir" 2>/dev/null
+
+python convert_coco_yolo.py "$train_anns" "$image_dir"
+python convert_coco_yolo.py "$validation_anns" "$image_dir"
+
+# Download pretrained weight
+wget https://pjreddie.com/media/files/darknet53.conv.74 -O "$PWD/darknet/build/darknet/x64/darknet53.conv.74"
