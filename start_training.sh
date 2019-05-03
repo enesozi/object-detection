b1bd629020a25b883359c658a3e1155354fa2a6c
enesozi <enesozipek05@gmail.com>
2019-05-03 11:17:40 +0300

    port removed

--- start_training.sh
+++ start_training.sh
@@ -1,2 +1,2 @@
 cd "$PWD/darknet/build/darknet/x64/"
-../../../darknet detector train data/thermal.data yolov3-thermal.cfg darknet53.conv.74 -gpus 0 -dont_show -mjpeg_port 8090 -map >> /home/tra_results.txt
+../../../darknet detector train data/thermal.data yolov3-thermal.cfg darknet53.conv.74 -gpus 0 -dont_show  -map >> /home/tra_results.txt
